5715479f581473d8ea6a45334e68f7f6d649fe2b
mezes1 <maxpiraliyev8@gmail.com>
2021-08-21 19:06:33 +0500

    +js

---
+++ js/js_amaliyot.js
@@ -0,0 +1,21 @@
+// Bu mening birinchi dasturim
+document.write("Hello World!<br>");
+document.write('Men MEZES');
+console.log('Hello');
+document.write('<hr>')
+
+
+let ism = 'Mezes';
+let yosh = 21;
+let im = "internet magazin"
+let me = "maqsadiga erishib"
+let uylangan = false;
+/*
+document.write(ism + " degan dasturchi bo'lgan ekan<br>")
+document.write('Uning yoshi ' + yosh +' da ekan<br>')
+document.write("Uning maqsadi " + im + " yaratish ekan<br>")
+document.write("Harakatlari natijasida " + me + "di")
+*/
+document.write(typeof(ism)+'<br>')
+document.write(typeof(yosh) + "<br>")
+document.write(typeof(uylangan))
